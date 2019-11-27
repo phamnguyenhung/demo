970ad53fa422f10277e1997b519a6f86d076a136
quangpv <quangpv.uit@gmail.com>
2019-11-27 16:19:30 +0700

    remove redundance code

--- app/src/main/java/com/example/myapplication/dto/ViewComponentDTOMapper.kt
+++ app/src/main/java/com/example/myapplication/dto/ViewComponentDTOMapper.kt
@@ -26,32 +26,29 @@ class ViewComponentDTOMapper {
 
     private fun MutableList<ViewComponent>.onTraversal(item: Item) =
         when (ComponentType.getTypeById(item.id)) {
-            ComponentType.SESSION_NAME -> {
-
-            }
-            ComponentType.PHONE_NUMBER_EDT -> this += PhoneComponent(
-                name = item.name,
-                title = item.title
-            ).apply {
-                isRequired = item.isRequired
-                param = item.param
-                id = item.id
-                type = ComponentType.PHONE_NUMBER_EDT
-            }
-
-            ComponentType.PLAIN_EDT -> this += PlainEdtComponent(
-                name = item.name
-            ).apply {
-                isRequired = item.isRequired
-                param = item.param
-                id = item.id
-                type = ComponentType.PHONE_NUMBER_EDT
-            }
-            ComponentType.NOTE_EDT -> this += NoteComponent(
-
-            )
-            else -> {
-
-            }
+            ComponentType.SESSION_NAME -> { }
+            ComponentType.PHONE_NUMBER_EDT -> this += toPhoneComponent(item)
+            ComponentType.PLAIN_EDT -> this += toPlain(item)
+            ComponentType.NOTE_EDT -> this += NoteComponent()
+            else -> { }
         }
+
+    private fun toPlain(item: Item) = PlainEdtComponent(
+        name = item.name
+    ).apply {
+        isRequired = item.isRequired
+        param = item.param
+        id = item.id
+        type = ComponentType.PHONE_NUMBER_EDT
+    }
+
+    private fun toPhoneComponent(item: Item) = PhoneComponent(
+        name = item.name,
+        title = item.title
+    ).apply {
+        isRequired = item.isRequired
+        param = item.param
+        id = item.id
+        type = ComponentType.PHONE_NUMBER_EDT
+    }
 }
